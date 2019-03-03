4a8972676220400fdbe24c6b7c7759970be34671
Danny Parker <dannyparker@dannys-imac.local>
2019-03-03 17:12:30 +0800

    Convert to MVC design Pattern.

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,19 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Danny Parker on 3/3/19.
+//  Copyright © 2019 Danny Parker. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+    
+    
+}
